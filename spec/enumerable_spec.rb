5fa4ba25978a3c00c5a17294edde96ac923e851c
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-02-12 17:29:08 +0300

    add more test cases.

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Enumerable do
   describe "#my_each" do
     it "should access all element in given array" do
       counter = 0
-      array = [1,2,3,4,5,6,7,8,9,10]
+      array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
       array.my_each do |e|
         expect(e).to eql(array[counter])
         counter += 1
@@ -91,38 +91,70 @@ RSpec.describe Enumerable do
   end
 
 
-    describe "#my_any?" do
-      it "Should return a boolean" do
-        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        expect(numbers.my_any? { |e| e.even? }).to be(true).or be(false)
-      end
-
-      it "should return true if any element provides the given block condition" do
-        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, -10]
-        expect(numbers.my_any? { |e| e < 0 }).to eql(true)
-      end
-
-      it "should return false if any element doesn't provide the given block condition" do
-        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        expect(numbers.my_any? { |e| e < 0 }).to eql(false)
-      end
+  describe "#my_any?" do
+    it "Should return a boolean" do
+      numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      expect(numbers.my_any? { |e| e.even? }).to be(true).or be(false)
     end
 
-    describe "#my_none?" do
-      it "should return a boolean" do
-        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        expect(numbers.my_none? { |e| e.even? }).to be(true).or be(false)
-      end
-
-      it "should return true if any element doesn't provide the given block condition" do
-        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        expect(numbers.my_none? { |e| e < 0 }).to eql(true)
-      end
-
-      it "should return false if any element provides the given block condition" do
-        numbers = [-3, -2, -1, 0, 1, 2, 3]
-        expect(numbers.my_none? { |e| e < 0 }).to eql(false)
-      end
+    it "should return true if any element provides the given block condition" do
+      numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, -10]
+      expect(numbers.my_any? { |e| e < 0 }).to eql(true)
     end
 
+    it "should return false if any element doesn't provide the given block condition" do
+      numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      expect(numbers.my_any? { |e| e < 0 }).to eql(false)
+    end
+  end
+
+  describe "#my_none?" do
+    it "should return a boolean" do
+      numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      expect(numbers.my_none? { |e| e.even? }).to be(true).or be(false)
+    end
+
+    it "should return true if any element doesn't provide the given block condition" do
+      numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      expect(numbers.my_none? { |e| e < 0 }).to eql(true)
+    end
+
+    it "should return false if any element provides the given block condition" do
+      numbers = [-3, -2, -1, 0, 1, 2, 3]
+      expect(numbers.my_none? { |e| e < 0 }).to eql(false)
+    end
+  end
+
+  describe "#my_count" do
+    it "should return 0 when array is empty" do
+      empty_array = []
+      expect(empty_array.my_count).to eql(0)
+    end
+
+    it "should return size of array" do
+      array = [1,2,3,4,5]
+      expect(array.my_count).to eql(array.length)
+      expect(array.my_count).to eql(5)
+    end
+  end
+
+  describe "#my_map" do
+    it "should return a new array processed by the given block" do
+      numbers = [1,2,3,4,5]
+      numbers_powed = numbers.my_map { |e| e = e * e }
+      expect(numbers_powed).to be_instance_of(Array)
+      expect(numbers_powed).to eql([1,4,9,16,25])
+    end
+  end
+
+  describe "#my_inject" do
+    it "should make calculation memoizated value" do
+      numbers = [1,2,3,4,5]
+      sum = numbers.inject { |memo, e| memo + e }
+      expect(sum).to eql(15)
+      words = %w{ cat sheep bear }
+      longest_word = words.inject { |memo, word| (memo.length > word.length)? memo : word }
+      expect(longest_word).to eql('sheep')
+    end
+  end
 end

e19232a37c9ec2dc36dce6932d1d340b30775cda
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-02-12 13:31:36 +0300

    add test cases with using mock object

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,5 +1,14 @@
 require('./lib/enumerable.rb')
 include Enumerable
+
+class MockObject
+  attr_reader :variable, :other_variable
+  def initialize variable = 'hello', other_variable = 'world'
+    @variable = variable
+    @other_variable = other_variable
+  end
+end
+
 RSpec.describe Enumerable do
   describe "#my_each" do
     it "should access all element in given array" do
@@ -11,11 +20,32 @@ RSpec.describe Enumerable do
       end
       expect(counter).to eql(array.length)
     end
+
+    it "should work with object data" do
+      object_array = [MockObject.new, MockObject.new, MockObject.new]
+      object_array.my_each do |e|
+        expect(e.variable).to eql('hello')
+        expect(e.other_variable).to eql('world')
+      end
+    end
+
   end
   describe "#my_each_with_index" do
     it "should get element by index" do
       array = [1,2,3,4,5,6,7,8,9,10]
       expect(array.my_each_with_index(0)).to eql(1)
+    end
+
+    it "should work with object data" do
+      object_array = [MockObject.new('first','object'),
+                      MockObject.new('second', 'object'),
+                      MockObject.new('third', 'object')]
+      expect(object_array.my_each_with_index(0).variable).to eql('first')
+      expect(object_array.my_each_with_index(0).other_variable).to eql('object')
+      expect(object_array.my_each_with_index(1).variable).to eql('second')
+      expect(object_array.my_each_with_index(1).other_variable).to eql('object')
+      expect(object_array.my_each_with_index(2).variable).to eql('third')
+      expect(object_array.my_each_with_index(2).other_variable).to eql('object')
     end
 
     it "should return nil when index is not found" do
